ede45ed385871755b1e263d1dc81bfbf02177908
Nicholas Johnson <nicholas.johnson78@gmail.com>
2022-12-24 17:02:56 -0500

    Initial commit

---
+++ src/components/DarkMode.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+<template>
+    <button
+        @click="toggleDark()"
+        class="rounded bg-indigo-500 hover:bg-indigo-400 text-gray-50 dark:bg-gray-900 border border-transparent dark:hover:border-[#646cff] py-2 px-6 text-base font-normal cursor-pointer transition-all duration-150">
+    Toggle Darkmode
+    </button>
+</template>
---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter', {
+  state: () => ({ count: 0 }),
+  getters: {
+    double: state => state.count * 2,
+  },
+  actions: {
+    increment() {
+      this.count++
+    },
+    reset() {
+      this.count = 0
+    },
+  },
+  persist: false,
+})
