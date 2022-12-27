4fc79ba7fccfc05dfa6c08e8e2fc8221286598fc
n1ch0la5 <nicholas.johnson78@gmail.com>
2022-12-26 20:40:23 -0500

    updated mobile padding

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 const year: number = new Date().getFullYear()
 </script>
 <template>
-  <div class="max-w-7xl mx-auto p-12 pb-0 shadow-2xl bg-[#e7e8e9] border border-gray-400 min-h-screen flex flex-col justify-between">
+  <div class="max-w-7xl mx-auto p-8 md:p-12 pb-0 shadow-2xl bg-[#e7e8e9] border border-gray-400 min-h-screen flex flex-col justify-between">
     <div class="">
       <nav class="mb-6 pl-4">
         <router-link to="/" exact>Home</router-link> |
