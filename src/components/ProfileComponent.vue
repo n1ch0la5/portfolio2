acdf79813e0fec3e748ec433934d49b16baade25
n1ch0la5 <nicholas.johnson78@gmail.com>
2024-07-10 11:37:31 -0400

    image optimization

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -2,7 +2,7 @@
     <div class="w-full relative text-gray-300 rounded-xl lg:w-5/12 p-12 lg:p-8 bg-gradient-to-tr from-gray-900 to-gray-800 drop-shadow-[0_15px_15px_rgba(0,0,0,0.50)]">
       <div class="h-full pt-24 flex flex-col justify-between">
         <div class="text-center">
-          <img src="../assets/images/nick-gray.png" alt="Nick Johnson" class="absolute -top-8 left-0 right-0 mx-auto block border-2 border-gray-800 rounded-full w-36 h-36 shadow">
+          <img src="../assets/images/nick-min.png" alt="Nick Johnson" class="absolute -top-8 left-0 right-0 mx-auto block border-2 border-gray-800 rounded-full w-36 h-36 shadow">
           <p class="font-bold">Nicholas S. Johnson</p>
           <p class="text-sm mb-3">Full Stack Engineer</p>
           <p class="text-base text-center italic mb-4">"With my extensive experience and commitment to excellence, I’m confident I can be a valuable asset to any team. I’m excited about the opportunity to keep creating innovative solutions for clients."</p>
